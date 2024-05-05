ac9587200365f77dfc24063bb08059d08807bd0b
ISHAN <ishan17052002@gmail.com>
2024-05-05 12:39:00 +0530

    hel

--- server/app.js
+++ server/app.js
@@ -27,11 +27,11 @@ database.connect()
 app.use(express.json())
 app.use(cookieParser())
 // same port run -> after npm run build
-const path = require("path")
-const { loadEnvFile } = require("process")
-const _dirname = path.dirname("")
-const buildpath = path.join(_dirname, "../build")
-app.use(express.static(buildpath))
+// const path = require("path")
+// const { loadEnvFile } = require("process")
+// const _dirname = path.dirname("")
+// const buildpath = path.join(_dirname, "../build")
+// app.use(express.static(buildpath))
 app.use(
   cors({
     origin: "*",
