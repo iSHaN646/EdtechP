251e22e3b0d9e1faab83db962cf4a6c5df43f327
ISHAN <ishan17052002@gmail.com>
2024-05-05 14:05:52 +0530

    jdhd

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,4 +1,4 @@
-const BASE_URL = process.env.REACT_APP_BASE_URL //deployed backend base url
+const BASE_URL = "https://edtechp.onrender.com" //deployed backend base url
 
 // AUTH ENDPOINTS
 export const endpoints = {
